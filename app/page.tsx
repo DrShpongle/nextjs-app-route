d885bae47032dba35179eb1b164a4d1deefbf818
DrShpongle <evgeniy.nagalskiy@gmail.com>
2023-11-23 13:27:45 +0100

    auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import AcmeLogo from "@/app/ui/acme-logo";
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
 
 import Link from "next/link";
@@ -11,7 +11,9 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <h2 className={`${lusitana.className} antialiased text-6xl`}>Test Heading</h2>
+          <h2 className={`${lusitana.className} antialiased text-6xl`}>
+            Test Heading
+          </h2>
           <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{" "}
